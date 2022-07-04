eece9b2190bdc688f5100b944e6fc6c6436aec4d
denysbraun85 <denysbraun1985@gmail.com>
2022-07-04 16:24:36 +0300

    [feature] - continue lessons, video stoped at 24-01, registrated on another weather app, add axios response function

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -1,10 +1,10 @@
 import {useState} from "react";
 import axios from "axios";
 
-const BASE_URL = 'https://api.open-meteo.com/v1/forecast';
-const BASE_URL2 = 'https://api.open-meteo.com/v1/forecast';
+const BASE_URL = 'https://www.metaweather.com/api/location';
+// const BASE_URL2 = 'https://api.open-meteo.com/v1/forecast';
 const CROSS_DOMAIN = 'https://the-ultimate-api-challenge.herokuapp.com';
-const REQUEST_URL = `${CROSS_DOMAIN}${BASE_URL}`;
+const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`;
 
 const useForecast = () => {
     const [isError, setIsError] = useState(false);
@@ -12,8 +12,13 @@ const useForecast = () => {
     const [forecast, setForecast] = useState(null);
 
     // call the api
-    const submitRequest = location => {
-        axios();
+    const submitRequest = async location => {
+        //1. get woeid
+        const response = await axios(`${REQUEST_URL}/search`, {params: {query: location}});
+        //2. get wheather
+        console.log(response);
+        console.log({response});
+
     };
 
     return {
